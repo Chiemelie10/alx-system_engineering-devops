f1fa7f4a817d28d79d2817086580fdf9c24b7f5e
Chiemelie10 <chiemelieeze@yahoo.com>
2023-03-25 11:11:36 +0000

    Task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This script creates a file in /tmp with the following given requirements.
+
+file { 'school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
